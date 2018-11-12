a8cbbb585756c590a35bc432a75b41f1989b3332
half2t <half2t@gmail.com>
2018-11-12 17:02:50 +0300

    flask flash

--- web_server.py
+++ web_server.py
@@ -1,34 +1,35 @@
 #!/usr/bin/env python2
 
 
-from flask import Flask, render_template, request
-from flask import redirect, url_for, jsonify, make_response
+from flask import Flask, render_template, request, session
+from flask import redirect, url_for, jsonify, flash
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Categorie, Item, User
 import hashlib
 
 app = Flask(__name__)
+app.secret_key = "mohamed%$#^&^%$ahmed@@#||L?>"
 
 engine = create_engine('sqlite:///catalog.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
-session = DBSession()
+dbsession = DBSession()
 
 
 # Home Page
 @app.route('/')
 @app.route('/index')
 def index():
-    categories = session.query(Categorie).all()
+    categories = dbsession.query(Categorie).all()
     return render_template('index.html', categories=categories)
 
 
 @app.route('/Items/<int:categorie_id>')
 def Items(categorie_id):
-    categorie = session.query(Categorie).filter_by(id=categorie_id).one()
-    items = session.query(Item).filter_by(categorie_id=categorie_id).all()
+    categorie = dbsession.query(Categorie).filter_by(id=categorie_id).one()
+    items = dbsession.query(Item).filter_by(categorie_id=categorie_id).all()
     return render_template('Items.html', categorie=categorie, items=items)
 
 
@@ -42,23 +43,26 @@ def Registration():
         user = User(name=request.form['fullname'], 
                     username=request.form['username'], 
                     passwd=hash_object.hexdigest())
-        session.add(user)
-        session.commit()
+        dbsession.add(user)
+        dbsession.commit()
+        flash('Operation completed successfully')
         return redirect(url_for('Login'))
     
 # Login  
 @app.route('/Login', methods=['GET','POST'])
 def Login():
     if request.method == 'GET':
+        session.pop('UserId', None)
+        session.pop('Name', None)
         return render_template('Login.html')
     if request.method == 'POST':
         hash_object = hashlib.md5(request.form['passwd'].encode())
-        user = session.query(User).filter_by(username=request.form['username'],
+        user = dbsession.query(User).filter_by(username=request.form['username'],
                             passwd=hash_object.hexdigest()).first()
         if user:
-          resp = make_response(redirect(url_for('AddCategorie')))
-          resp.set_cookie('user_cookie', user.id)
-          return resp
+          session['UserId'] = user.id
+          session['Name'] = user.name
+          return redirect(url_for('AddCategorie'))
         else:
             return redirect(url_for('Login'))
         
@@ -66,124 +70,126 @@ def Login():
 # Logout
 @app.route('/Logout')
 def Logout():
-    resp = make_response(redirect(url_for('Login')))
-    resp.set_cookie('user_cookie', '', expires=0)
-    return resp
+    session.pop('UserId', None)
+    session.pop('Name', None)
+    return redirect(url_for('index'))
 
 
 # Insert new Categorie
 @app.route('/NewCateogrie', methods=['GET','POST'])
 def AddCategorie():
-    user_cookie = request.cookies.get('user_cookie')
-    if user_cookie is None:
+    if not session.get('UserId'):
         return redirect(url_for('Login'))
     if request.method == 'GET':
-        categories = session.query(Categorie).filter_by(
-                created_by=user_cookie).all()
+        categories = dbsession.query(Categorie).filter_by(
+                created_by=session['UserId']).all()
         return render_template('NewCategorie.html', categories=categories)
     if request.method == 'POST':
         categorie = Categorie(name=request.form['categoriename']
-        ,created_by=user_cookie)
-        session.add(categorie)
-        session.commit()
+        ,created_by=session['UserId'])
+        dbsession.add(categorie)
+        dbsession.commit()
+        flash('Operation completed successfully')
         return redirect(url_for('AddCategorie'))
     
     
 # Insert new Item
 @app.route('/NewItem/<int:categorie_id>', methods=['GET','POST'])
 def AddItem(categorie_id):
-    user_cookie = request.cookies.get('user_cookie')
-    if user_cookie is None:
+    if not session.get('UserId'):
         return redirect(url_for('Login'))
     if request.method == 'GET':
-        categorie = session.query(Categorie).filter_by(id=categorie_id).one()
-        items = session.query(Item).filter_by(categorie_id=categorie_id).all()
+        categorie = dbsession.query(Categorie).filter_by(id=categorie_id).one()
+        items = dbsession.query(Item).filter_by(
+                categorie_id=categorie_id).all()
         return render_template('CategorieItems.html', categorie=categorie,
                                items=items)
     if request.method == 'POST':
         item = Item(name=request.form['itemname']
         , description=request.form['description'], categorie_id=categorie_id)
-        session.add(item)
-        session.commit()
+        dbsession.add(item)
+        dbsession.commit()
+        flash('Operation completed successfully')
         return redirect(url_for('AddItem', categorie_id=categorie_id))
     
     
 # Edit Categorie
 @app.route('/EditCateogrie/<int:categorie_id>', methods=['GET','POST'])
 def EditCategorie(categorie_id):
-    user_cookie = request.cookies.get('user_cookie')
-    if user_cookie is None:
+    if not session.get('UserId'):
         return redirect(url_for('Login'))
     if request.method == 'GET':
-        categorie = session.query(Categorie).filter_by(id=categorie_id,
-                                 created_by=user_cookie).one()
+        categorie = dbsession.query(Categorie).filter_by(id=categorie_id,
+                                 created_by=session['UserId']).one()
         return render_template('EditCategorie.html', categorie=categorie)
     if request.method == 'POST':
-        categorie = session.query(Categorie).filter_by(id=categorie_id).one()
+        categorie = dbsession.query(Categorie).filter_by(id=categorie_id).one()
         categorie.name = request.form['categoriename']
-        session.add(categorie)
-        session.commit()
+        dbsession.add(categorie)
+        dbsession.commit()
+        flash('Operation completed successfully')
         return redirect(url_for('AddCategorie'))
     
     
 # Edit Item
 @app.route('/EditItem/<int:item_id>', methods=['GET','POST'])
 def EditItem(item_id):
-    user_cookie = request.cookies.get('user_cookie')
-    if user_cookie is None:
+    if not session.get('UserId'):
         return redirect(url_for('Login'))
     if request.method == 'GET':
-        item = session.query(Item).filter_by(id=item_id).one()
+        item = dbsession.query(Item).filter_by(id=item_id).one()
         return render_template('EditItem.html', item=item)
     if request.method == 'POST':
-        item = session.query(Item).filter_by(id=item_id).one()
+        item = dbsession.query(Item).filter_by(id=item_id).one()
         item.name = request.form['itemname']
         item.description = request.form['item_description']
-        session.add(item)
-        session.commit()
+        dbsession.add(item)
+        dbsession.commit()
+        flash('Operation completed successfully')
         return redirect(url_for('AddItem', categorie_id=item.categorie_id))
     
  
 # Delete Categorie
 @app.route('/DeleteCategorie/<int:categorie_id>')
 def DeleteCategorie(categorie_id):
-    user_cookie = request.cookies.get('user_cookie')
-    if user_cookie is None:
+    if not session.get('UserId'):
         return redirect(url_for('Login'))
-    categorie = session.query(Categorie).filter_by(id=categorie_id).one()
-    items = session.query(Item).filter_by(categorie_id=categorie_id).all()
-    for item in items:
-     session.delete(item)
-    session.commit()
-    session.delete(categorie)
-    session.commit()
+    categorie = dbsession.query(Categorie).filter_by(id=categorie_id).one()
+    items = dbsession.query(Item).filter_by(categorie_id=categorie_id).all()
+    if items:
+      for item in items:
+        dbsession.delete(item)
+    dbsession.commit()
+    dbsession.delete(categorie)
+    dbsession.commit()
+    flash('Operation completed successfully')
     return redirect(url_for('AddCategorie'))
 
 
 # Delete Item
 @app.route('/DeleteItem/<int:item_id>')
 def deleteItem(item_id):
-    user_cookie = request.cookies.get('user_cookie')
-    if user_cookie is None:
+    if not session.get('UserId'):
         return redirect(url_for('Login'))
-    item = session.query(Item).filter_by(id=item_id).one()
+    item = dbsession.query(Item).filter_by(id=item_id).one()
     categorie_id = item.categorie_id
-    session.delete(item)
-    session.commit()
+    dbsession.delete(item)
+    dbsession.commit()
+    flash('Operation completed successfully')
     return redirect(url_for('AddItem', categorie_id=categorie_id))
 
 
 # Return List of categories as Json
 @app.route('/CategoriesJSON')
 def CategoriesJSON():
-    categories = session.query(Categorie).all()
+    categories = dbsession.query(Categorie).all()
     return jsonify(categories=[i.serialize for i in categories])
 
 
 # Return List of items as Json
 @app.route('/ItemsJSON/<int:categorie_id>')
 def ItemsJSON(categorie_id):
-    items = session.query(Item).filter_by(categorie_id=categorie_id).all()
+    items = dbsession.query(Item).filter_by(categorie_id=categorie_id).all()
     return jsonify(items=[i.serialize for i in items])
     
 
